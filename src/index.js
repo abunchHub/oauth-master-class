c31f5ce4bc3bcb7a5cadc1b9f04ee12f2d6a78e2
abunchHub <113227482+abunchHub@users.noreply.github.com>
2024-04-09 11:43:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ window.onload = () => {
   document.getElementById("button").onclick = () => {
     window.YaAuthSuggest.init(
       {
-        client_id: "c46f0c53093440c39f12eff95a9f2f93",
+        client_id: "d7ace4e309a94c098054e6917e51d76e",
         response_type: "token",
-        redirect_uri: "https://examplesite.com/suggest/token",
+        redirect_uri: "https://oauth-master-class-ruby.vercel.app/token.html",
       },
-      "https://examplesite.com",
+      "https://oauth-master-class-ruby.vercel.app/",
       {
         view: "button",
         parentId: "buttonContainer",
